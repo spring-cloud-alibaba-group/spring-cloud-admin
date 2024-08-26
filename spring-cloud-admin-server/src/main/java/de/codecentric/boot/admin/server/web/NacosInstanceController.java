f5283ee4cfa99348bd086ceae43aa6985c8b1864
amore <83866275+amoreZgx1n@users.noreply.github.com>
2024-08-26 14:53:23 +0800

    nacos web server

---
+++ spring-cloud-admin-server/src/main/java/de/codecentric/boot/admin/server/web/NacosInstanceController.java
@@ -0,0 +1,45 @@
+package de.codecentric.boot.admin.server.web;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceManager;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+
+import de.codecentric.boot.admin.server.domain.entities.InstanceMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/search")
+public class NacosInstanceController {
+
+    @Autowired
+    private NacosServiceManager nacosServiceManager;
+    @Autowired
+    private NacosDiscoveryProperties nacosDiscoveryProperties;
+    @Autowired
+    private Registration registration;
+
+    @GetMapping("/{serviceName}/metadata")
+    public List<InstanceMetadata> getInstanceMetadataByServiceName(@PathVariable String serviceName) throws NacosException {
+        List<InstanceMetadata> serviceMetadata = new ArrayList<>();
+        List<Instance> instances = nacosServiceManager.getNamingService().getAllInstances(serviceName);
+        for (Instance instance : instances) {
+            InstanceMetadata metadata = new InstanceMetadata(serviceName, instance.getMetadata());
+            serviceMetadata.add(metadata);
+        }
+        return serviceMetadata;
+    }
+
+    @GetMapping("/{serviceName}")
+    public List<Instance> getInstanceByServiceName(@PathVariable String serviceName) throws NacosException {
+        List<InstanceMetadata> serviceMetadata = new ArrayList<>();
+        List<Instance> instances = nacosServiceManager.getNamingService().getAllInstances(serviceName);
+        return instances;
+    }
+}
